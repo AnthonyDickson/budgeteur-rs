195f68bf40ab9a78e063495582fe54a78c6d4ddc
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-07-23 12:31:38 +1200

    Gracefully shutdown on ctrl+c

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, Router, routing::get};
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
     let app = Router::new().route("/", get(handler));
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
