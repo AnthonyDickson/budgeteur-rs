40e8abb8e5e25f7d1d45ccda501d77be81fc413c
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-09-01 13:13:06 +1200

    Remove unused variants from `AuthError`.

--- src/auth.rs
+++ src/auth.rs
@@ -35,20 +35,14 @@ pub struct Credentials {
 
 #[derive(Debug)]
 pub enum AuthError {
-    WrongCredentials,
-    MissingCredentials,
-    TokenCreation,
-    InvalidToken,
+    InvalidCredentials,
     InternalError,
 }
 
 impl IntoResponse for AuthError {
     fn into_response(self) -> Response<Body> {
         let (status, error_message) = match self {
-            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
-            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
-            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
-            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+            AuthError::InvalidCredentials => (StatusCode::UNAUTHORIZED, "Invalid credentials"),
             AuthError::InternalError => {
                 (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
             }
@@ -67,9 +61,7 @@ impl IntoResponse for AuthError {
 /// # Errors
 ///
 /// This function will return an error in a few situtations.
-/// - The email is empty.
 /// - The email does not belong to a registered user.
-/// - The password is empty.
 /// - The password is not correct.
 /// - An internal error occurred when verifying the password.
 pub async fn sign_in(
@@ -79,7 +71,7 @@ pub async fn sign_in(
 ) -> Result<PrivateCookieJar, AuthError> {
     let user = User::select(&user_data.email, &state.db_connection().lock().unwrap()).map_err(
         |e| match e {
-            DbError::NotFound => AuthError::WrongCredentials,
+            DbError::NotFound => AuthError::InvalidCredentials,
             _ => {
                 tracing::error!("Error matching user: {e:?}");
                 AuthError::InternalError
@@ -99,7 +91,7 @@ pub async fn sign_in(
 
                 Ok(updated_jar)
             } else {
-                Err(AuthError::WrongCredentials)
+                Err(AuthError::InvalidCredentials)
             }
         })?
 }
@@ -119,12 +111,12 @@ pub(crate) fn set_auth_cookie(jar: PrivateCookieJar, user_id: UserID) -> Private
 
 pub(crate) fn get_user_id_from_auth_cookie(jar: PrivateCookieJar) -> Result<UserID, AuthError> {
     match jar.get(COOKIE_USER_ID) {
-        None => Err(AuthError::InvalidToken),
+        None => Err(AuthError::InvalidCredentials),
         Some(user_id_cookie) => user_id_cookie
             .value_trimmed()
             .parse()
             .map(UserID::new)
-            .map_err(|_| AuthError::InvalidToken),
+            .map_err(|_| AuthError::InvalidCredentials),
     }
 }
 
