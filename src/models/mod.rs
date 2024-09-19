1c4aa8318ffc517851c877d89f63a96a91879d1a
Anthony Dickson <anthony.dickson9656@gmail.com>
2024-09-19 21:23:12 +1200

    Remove unused types

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,15 +2,11 @@
 
 pub use category::{Category, CategoryName, NewCategory};
 pub use password::{PasswordError, PasswordHash, ValidatedPassword};
-pub use recurring_transaction::{Frequency, NewRecurringTransaction, RecurringTransaction};
-pub use savings_ratio::{NewSavingsRatio, Ratio, SavingsRatio};
 pub use transaction::{NewTransaction, Transaction};
 pub use user::{User, UserError, UserID};
 
 mod category;
 mod password;
-mod recurring_transaction;
-mod savings_ratio;
 mod transaction;
 mod user;
 pub type DatabaseID = i64;
