c94e6b2001f53704e22a5106a4dd8080b53b86d3
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-05-22 20:23:25 +1200

    Add HTML template for balances view

--- src/routes/views/balances.rs
+++ src/routes/views/balances.rs
@@ -1,13 +1,31 @@
 //! Displays accounts and their balances.
 
 use askama_axum::IntoResponse;
-use axum::{Extension, extract::State, http::StatusCode, response::Response};
+use askama_axum::Template;
+use axum::{Extension, extract::State, response::Response};
 
 use crate::{
     AppState,
     models::UserID,
+    routes::{
+        endpoints,
+        navigation::{NavbarTemplate, get_nav_bar},
+    },
     stores::{CategoryStore, TransactionStore, UserStore},
 };
+
+struct Balance<'a> {
+    account: &'a str,
+    balance: f64,
+}
+
+/// Renders the balances page.
+#[derive(Template)]
+#[template(path = "views/balances.html")]
+struct BalancesTemplate<'a> {
+    nav_bar: NavbarTemplate<'a>,
+    balances: &'a [Balance<'a>],
+}
 
 /// Renders the page for creating a transaction.
 pub async fn get_balances_page<C, T, U>(
@@ -19,11 +37,18 @@ where
     T: TransactionStore + Send + Sync,
     U: UserStore + Send + Sync,
 {
-    (StatusCode::OK, "Balances Page").into_response()
+    BalancesTemplate {
+        nav_bar: get_nav_bar(endpoints::BALANCES_VIEW),
+        balances: &[Balance {
+            account: "1234-5678-9101-12",
+            balance: 1234.56,
+        }],
+    }
+    .into_response()
 }
 
 mod tests {
-    use axum::{Extension, extract::State, http::StatusCode};
+    use axum::{Extension, extract::State, http::StatusCode, response::Response};
 
     use crate::{
         AppState,
@@ -31,6 +56,31 @@ mod tests {
         routes::views::balances::get_balances_page,
         stores::{CategoryStore, TransactionStore, UserStore},
     };
+
+    #[tokio::test]
+    async fn test_get_balances_view() {
+        let state = AppState::new(
+            "foo",
+            DummyCategoryStore {},
+            DummyTransactionStore {},
+            DummyUserStore {},
+        );
+        let user_id = UserID::new(1);
+
+        let response = get_balances_page(State(state), Extension(user_id)).await;
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_content_type(&response, "text/html; charset=utf-8");
+    }
+
+    #[track_caller]
+    fn assert_content_type(response: &Response, content_type: &str) {
+        let content_type_header = response
+            .headers()
+            .get("content-type")
+            .expect("content-type header missing");
+        assert_eq!(content_type_header, content_type);
+    }
 
     struct DummyCategoryStore;
 
@@ -126,20 +176,5 @@ mod tests {
         ) -> Result<crate::models::User, crate::Error> {
             todo!()
         }
-    }
-
-    #[tokio::test]
-    async fn test_get_balances_view() {
-        let state = AppState::new(
-            "foo",
-            DummyCategoryStore {},
-            DummyTransactionStore {},
-            DummyUserStore {},
-        );
-        let user_id = UserID::new(1);
-
-        let response = get_balances_page(State(state), Extension(user_id)).await;
-
-        assert_eq!(response.status(), StatusCode::OK);
     }
 }
