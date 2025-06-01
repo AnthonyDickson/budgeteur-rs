5df720b133afa533f9f2c096ac059988896a884b
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-06-01 16:35:39 +1200

    Add CLI tool to reset password

--- TODO.md
+++ TODO.md
@@ -8,10 +8,3 @@
   Refer to [v3.tailwindcss.com/docs/reusing-styles](https://v3.tailwindcss.com/docs/reusing-styles) and [v3.tailwindcss.com/docs/adding-custom-styles](https://v3.tailwindcss.com/docs/adding-custom-styles).
 - Fix broken [icon](./static/seal.png).
 - Refactor common testing functions into a separate module.
-- Refactor app to assume single user
-  - Simpler and all I need!
-  - Log in flow
-    - Log in page should have link for "forgot your password?". This should
-      give instructions to run a CLI script that will change the password in the
-      database. The CLI script should use a similar flow to the registration
-      page for setting up a password.
---
+++ src/bin/reset_password.rs
@@ -0,0 +1,177 @@
+use std::{
+    error::Error,
+    io::{self},
+    path::Path,
+    process::exit,
+    sync::{Arc, Mutex},
+};
+
+use bcrypt::DEFAULT_COST;
+use budgeteur_rs::{
+    models::{User, UserID},
+    stores::{UserStore, sqlite::SQLiteUserStore},
+};
+use clap::Parser;
+use rusqlite::Connection;
+
+use budgeteur_rs::models::{PasswordHash, ValidatedPassword};
+
+/// A utility for changing the password for a registered user.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// File path to the application SQLite database.
+    #[arg(long)]
+    db_path: String,
+}
+
+/// Create and populate a database for manual testing.
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let db_path = Path::new(&args.db_path);
+    validate_db_path(db_path);
+
+    let user = get_user(db_path);
+    let password_hash = match get_new_password_hash() {
+        Some(password_hash) => password_hash,
+        None => return Ok(()),
+    };
+    update_password(db_path, user, password_hash)?;
+
+    Ok(())
+}
+
+fn get_user(db_path: &Path) -> User {
+    println!("Loading user from from {db_path:#?}");
+
+    let conn =
+        Connection::open(db_path).expect(&format!("Could not open the database at {db_path:?}"));
+    let conn = Arc::new(Mutex::new(conn));
+    let store = SQLiteUserStore::new(conn);
+
+    let user_count = store
+        .count()
+        .expect(&format!("Could not get count of users in {db_path:?}"));
+
+    if user_count != 1 {
+        print_error(format!(
+            "Expected one user in {db_path:#?}, found {user_count}."
+        ));
+        exit(1);
+    }
+
+    store
+        .get(UserID::new(1))
+        .expect("Could not get user with ID=1 in {db_path}.")
+}
+
+fn validate_db_path(db_path: &Path) {
+    match db_path.extension() {
+        None => {
+            print_error("Database path must include a file extension (e.g., 'my_database.db').");
+            exit(1);
+        }
+        Some(extension) if extension.is_empty() => {
+            print_error("Database path must include a file extension (e.g., 'my_database.db').");
+            exit(1);
+        }
+        _ => {}
+    }
+
+    if !db_path.is_file() {
+        eprintln!("File does not exist at {db_path:#?}!");
+        exit(1);
+    }
+}
+
+fn get_new_password_hash() -> Option<PasswordHash> {
+    loop {
+        println!("");
+
+        let first_password = match rpassword::prompt_password("Enter a new password: ") {
+            Ok(string) => string,
+            Err(error) if error.kind() == io::ErrorKind::UnexpectedEof => {
+                return None;
+            }
+            Err(error) => {
+                print_error(format!("Could not read password from stdin: {error}"));
+                return None;
+            }
+        };
+
+        if let Err(error) = ValidatedPassword::new(&first_password) {
+            print_error(error);
+            continue;
+        }
+
+        let second_password = match rpassword::prompt_password("Enter the same password again: ") {
+            Ok(string) => string,
+            Err(error) if error.kind() == io::ErrorKind::UnexpectedEof => {
+                return None;
+            }
+            Err(error) => {
+                print_error(format!("Could not read password from stdin: {error}"));
+                return None;
+            }
+        };
+
+        if first_password != second_password {
+            print_error("Passwords must match, try again.");
+            continue;
+        }
+
+        let password_hash = match PasswordHash::from_raw_password(&first_password, DEFAULT_COST) {
+            Ok(password_hash) => password_hash,
+            Err(error) => {
+                print_error(format!("Could not hash password: {error}. Try again."));
+                continue;
+            }
+        };
+
+        return Some(password_hash);
+    }
+}
+
+fn print_error(error: impl ToString) {
+    eprintln!(
+        "\x1b[31;1m{}\x1b[0m",
+        capitalise_first_char(&error.to_string())
+    )
+}
+
+/// From https://crates.io/crates/capitalize
+fn capitalise_first_char(string: &str) -> String {
+    let mut chars = string.chars();
+    let Some(first) = chars.next() else {
+        return String::with_capacity(0);
+    };
+    first.to_uppercase().chain(chars).collect()
+}
+
+fn update_password(
+    db_path: &Path,
+    user: User,
+    password: PasswordHash,
+) -> Result<(), rusqlite::Error> {
+    let mut conn = Connection::open(db_path)?;
+    let transaction = conn.transaction()?;
+
+    let rows_affected = transaction.execute(
+        "UPDATE user SET password = ?1 WHERE user.id = ?2;",
+        (&password.to_string(), &user.id().as_i64()),
+    )?;
+
+    if rows_affected != 1 {
+        print_error(format!(
+            "Updating password affected {rows_affected} user(s), expected 1. Rolling back..."
+        ));
+        transaction.rollback()?;
+        return Err(rusqlite::Error::StatementChangedRows(rows_affected));
+    }
+
+    transaction.commit()?;
+
+    println!("Password updated successfully!");
+
+    Ok(())
+}
