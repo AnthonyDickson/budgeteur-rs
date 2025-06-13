7309da7e0e83b90f1bbe684e6227e56b665bb043
Anthony Dickson <anthony.dickson9656@gmail.com>
2025-06-13 15:49:01 +1200

    Check that transactions page displays correct transactions when paged

--- src/routes/views/transactions.rs
+++ src/routes/views/transactions.rs
@@ -29,9 +29,12 @@ struct TransactionsTemplate<'a> {
     import_transaction_route: Uri,
 }
 
+/// Controls paginations of transactions table.
 #[derive(Deserialize)]
 pub struct Pagination {
+    /// The page number to display. Starts from 1.
     pub page: Option<u32>,
+    /// The maximum number of transactions to display per page.
     pub per_page: Option<u32>,
 }
 
@@ -48,7 +51,7 @@ where
     let Pagination { page, per_page } = query_params;
 
     let (limit, offset) = match (page, per_page) {
-        (Some(page), Some(per_page)) => (page * per_page, per_page),
+        (Some(page), Some(per_page)) => ((page - 1) * per_page, per_page),
         _ => (20, 0),
     };
 
@@ -85,7 +88,7 @@ mod transactions_route_tests {
         http::StatusCode,
         response::Response,
     };
-    use scraper::Html;
+    use scraper::{ElementRef, Html, Selector, selectable::Selectable};
 
     use crate::{
         Error,
@@ -186,26 +189,36 @@ mod transactions_route_tests {
     #[tokio::test]
     async fn displays_paged_data() {
         let mut transactions = Vec::new();
-        for i in 0..20 {
-            transactions.push(Transaction::build(i as f64).finalise(i));
+        let mut want = Vec::new();
+        let page = 2;
+        let per_page = 4;
+        for i in 0..20_u32 {
+            let transaction = Transaction::build(i as f64).finalise(i as i64);
+            transactions.push(transaction.clone());
+
+            if i >= (page - 1) * per_page && i < page * per_page {
+                want.push(transaction);
+            }
         }
         let state = TransactionsViewState {
-            transaction_store: StubTransactionStore { transactions },
+            transaction_store: StubTransactionStore {
+                transactions: transactions.clone(),
+            },
         };
 
         let response = get_transactions_page(
             State(state),
             Query(Pagination {
-                page: Some(2),
-                per_page: Some(4),
+                page: Some(page),
+                per_page: Some(per_page),
             }),
         )
         .await;
 
-        // TODO: parse html
-        // TODO: check for table
-        // TODO: check for correct row count
-        // TODO: check correct transactions
+        let html = parse_html(response).await;
+        assert_valid_html(&html);
+        let table = must_get_table(&html);
+        assert_table_has_transactions(table, &want);
         // TODO: check for pagination indicator
         // TODO: check that pagination indicator displays up ten pages
         // TODO: check that pagination indicator displays current page in
@@ -216,6 +229,46 @@ mod transactions_route_tests {
         //  middle if current page > max pages and current page <= (page count - max pages)
         // TODO: check that previous page link is rendered if current page > 0
         // TODO: check that next page link is rendered if current page < page count
+    }
+
+    async fn parse_html(response: Response) -> Html {
+        let body = response.into_body();
+        let body = axum::body::to_bytes(body, usize::MAX)
+            .await
+            .expect("Could not get response body");
+        let text = String::from_utf8_lossy(&body).to_string();
+
+        Html::parse_document(&text)
+    }
+
+    #[track_caller]
+    fn must_get_table(html: &Html) -> ElementRef {
+        html.select(&scraper::Selector::parse("table").unwrap())
+            .next()
+            .expect("No table found")
+    }
+
+    #[track_caller]
+    fn assert_table_has_transactions(table: ElementRef, transactions: &[Transaction]) {
+        let row_selector = Selector::parse("tbody tr").unwrap();
+        let table_rows: Vec<ElementRef<'_>> = table.select(&row_selector).collect();
+
+        assert_eq!(table_rows.len(), transactions.len());
+
+        let th_selector = Selector::parse("th").unwrap();
+        for (i, (row, want)) in table_rows.iter().zip(transactions).enumerate() {
+            let th = row
+                .select(&th_selector)
+                .next()
+                .expect(&format!("Could not find th element in table row {i}"));
+
+            let id_str = th.text().collect::<String>();
+            let got_id: i64 = id_str.trim().parse().expect(&format!(
+                "Could not parse ID {id_str} on table row {i} as integer"
+            ));
+
+            assert_eq!(got_id, want.id());
+        }
     }
 
     async fn get_response_body_text(response: Response) -> String {
