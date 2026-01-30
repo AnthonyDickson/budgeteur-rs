c697d0170125add4a84138f64ece616183c2c821
Anthony Dickson <anthony.dickson9656@gmail.com>
2026-01-30 19:08:00 +1300

    feat: Use alerts for error on registration form submission instead of redirect (#92)
    This leads to better UX compared to redirecting to an internal server
    error page and makes the behaviour consistent with forms in other parts
    of the website.
    
    Validation errors are still handled via error messages in the form.

--- src/internal_server_error.rs
+++ src/internal_server_error.rs
@@ -3,21 +3,8 @@ use axum::{
     http::StatusCode,
     response::{Html, IntoResponse, Response},
 };
-use axum_htmx::HxRedirect;
 
-use crate::{endpoints, html::error_view};
-
-/// Get a response that will redirect the client to the internal server error 500 page.
-///
-/// **Note**: This redirect is intended to be served as a response to a POST request initiated by HTMX.
-/// Route handlers using GET should use `axum::response::Redirect` to redirect via a response.
-pub(crate) fn get_internal_server_error_redirect() -> Response {
-    (
-        HxRedirect(endpoints::INTERNAL_ERROR_VIEW.to_owned()),
-        StatusCode::INTERNAL_SERVER_ERROR,
-    )
-        .into_response()
-}
+use crate::html::error_view;
 
 pub struct InternalServerError<'a> {
     pub description: &'a str,
